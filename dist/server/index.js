2163244221564cc39632bc35dd8407b02d9c1c3a
Carlos Galarza <carloslfu@gmail.com>
2018-03-05 19:05:54 -0500

    Se compila app

--- dist/server/index.js
+++ dist/server/index.js
@@ -4052,7 +4052,7 @@ const view = F => async (s) => {
         view_1.h('div', { class: style('description') }, [
             `Este estudio contempla `,
             view_1.h('b', s.num.toString()),
-            ` Startups Digitales en Colombia puede consultar la fuente `,
+            ` Startups Digitales en Colombia pueden consultar la fuente `,
             view_1.h('a', { attrs: { target: '_blank', rel: 'noopener', href: 'https://docs.google.com/spreadsheets/d/1gn-wJpq_kxhGbByp76Sc3drJxXNDAVRiNjJy87HJ7Uc/edit#gid=0' } }, 'acá'),
             ` dicho documento es de libre edición, mantenido y curado por los diferentes founders de la Comunidad Startup de manera permanente.`,
             ` Ultima actualización ${s.lastUpdated}.`,
